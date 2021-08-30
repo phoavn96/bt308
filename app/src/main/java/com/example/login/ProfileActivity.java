bd1e55ba613acae93694cbd03795367fa6228134
phoavn96 <phoavn96@gmail.com>
2021-08-30 20:09:00 +0700

    ok

---
+++ app/src/main/java/com/example/login/ProfileActivity.java
@@ -0,0 +1,53 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    private TextView tvUser;
+    private Button btEdit;
+    private static final int REQUEST_CODE =1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        tvUser = (TextView) findViewById(R.id.tvUserName);
+        btEdit = (Button) findViewById(R.id.btEdit);
+        btEdit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btEdit:
+                onEdit();
+                break;
+            default:break;
+        }
+
+    }
+
+    private void onEdit() {
+        Intent intent = new Intent(this, EditActivity.class);
+        intent.putExtra("USER_NAME",tvUser.getText().toString());
+        startActivityForResult(intent,REQUEST_CODE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == REQUEST_CODE){
+            if (resultCode == RESULT_OK){
+                String result = data.getStringExtra("RESULT");
+                tvUser.setText(result);
+            }
+        }
+    }
+}
